6ee040351f84455ef1bd61071326545d1f2af06d
Tran Le Trung Chanh <78064706+chanhtran2410@users.noreply.github.com>
2022-12-17 19:58:40 +0700

    Delete chatapp directory

--- chatapp/usr/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,  include
-from . import views
-
-urlpatterns = [
-    path("login/", views.loginView, name = 'login'),
-    path("register/", views.registerView, name = 'register'),
-]
-    
